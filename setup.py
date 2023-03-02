07732286144afead050b58e9ddf8fdff92c232c7
omidobeidzadeh <omidobeidzadeh@gmail.com>
2023-03-03 03:23:17 +0330

    initialCommitt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fixer-pak',
+      version='0.1',
+      description='Fixer service',
+      url='https://github.com/omidobeidzadeh/fixerpak',
+      author='omid',
+      author_email='omidobeidzadeh@gmail.com',
+      license='MIT',
+      packages=['fixer'],
+      zip_safe=False)
