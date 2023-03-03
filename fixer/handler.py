31238b7b8bdf8015a2616d2b3d9354f072eb76bb
omidobeidzadeh <omidobeidzadeh@gmail.com>
2023-03-03 03:59:20 +0330

    call-api

--- fixer/handler.py
+++ fixer/handler.py
@@ -1,2 +1,12 @@
-def get_rates():
-    print("get rates")
+import requests
+import json
+
+base_path = "https://api.apilayer.com/fixer/latest?base=usd&apikey="
+
+
+def get_rates(api_key):
+    response = requests.get(base_path + api_key)
+
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='fixer-pak',
-      version='0.1',
+      version='0.2',
       description='Fixer service',
       url='https://github.com/omidobeidzadeh/fixerpak',
       author='omid',
       author_email='omidobeidzadeh@gmail.com',
       license='MIT',
       packages=['fixer'],
+      install_requires=['requests'],
       zip_safe=False)
